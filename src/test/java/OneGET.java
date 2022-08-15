4b2a9d5f3ad656ceced12292796b5d0dc1f9979d
Rathod <arathod@luxoft.com>
2022-08-15 18:27:46 +0200

    test 2 added

--- src/test/java/OneGET.java
+++ src/test/java/OneGET.java
@@ -1,16 +1,26 @@
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import io.restassured.RestAssured;
+import static io.restassured.RestAssured.*;
 import io.restassured.response.Response;
 
 public class OneGET {
 	
 	@Test
 	void test_01() {
-		Response res = RestAssured.get("http://ip.jsontest.com/");
+		Response res = get("http://ip.jsontest.com/");
 		System.out.println(res.asString());
 		System.out.println(res.getStatusCode());
-		
+		 Assert.assertEquals(res.getStatusCode(), 200);
+	}
+	
+	@Test
+	void test_02() {
+	
+		 given().
+		     get("http://ip.jsontest.com/")
+		 .then()
+		     .statusCode(200);
 	}
 	
 }
